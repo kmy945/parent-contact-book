3759486e294a12b0eddafaf8c4300ed90581b58e
kmy945 <karenigma@gmail.com>
2026-07-20 20:18:35 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE="pcb-v6";
-const ASSETS=["./","./index.html","./preview.html","./manifest.webmanifest"];
+const CACHE="pcb-v7-5-hero-20260720";
+const ASSETS=["./","./index.html","./preview.html","./manifest.webmanifest","./cover.jpg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r;})
+  .catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
+});
