d3a4e0d9427ef44fba1592599864340c1bcbfaa1
kmy945 <karenigma@gmail.com>
2026-08-06 22:07:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="pcb-v9-20260806";
+const CACHE="pcb-v9-3-20260806";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./cover-clean.jpg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
