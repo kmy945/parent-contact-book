17a1bbaaf02885c28376bf8331b15e446eedcc41
kmy945 <karenigma@gmail.com>
2026-08-07 12:54:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-const CACHE='pcb-v12-network-first';
-self.addEventListener('install',event=>self.skipWaiting());
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
-});
+const CACHE='pcb-v13';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
